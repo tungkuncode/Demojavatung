e2095ec92ebe2baf79986da34497a52cf568ab78
tung <tungkuncode@gmail.com>
2022-12-14 14:43:25 +0700

    first commit

---
+++ src/vn/viettuts/SortStudentByGPA.java
@@ -0,0 +1,19 @@
+package vn.viettuts;
+
+import Demo.Student;
+import java.util.Comparator;
+
+/**
+ * SortStudentByGPA class
+ * 
+ * @author viettuts.vn
+ */
+public class SortStudentByGPA implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        if (student1.getGpa() > student2.getGpa()) {
+            return 1;
+        }
+        return -1;
+    }
+}
---
+++ src/vn/viettuts/SortStudentByName.java
@@ -0,0 +1,16 @@
+package vn.viettuts;
+
+import Demo.Student;
+import java.util.Comparator;
+
+/**
+ * SortStudentByName class
+ * 
+ * @author viettuts.vn
+ */
+public class SortStudentByName implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        return student1.getName().compareTo(student2.getName());
+    }
+}
